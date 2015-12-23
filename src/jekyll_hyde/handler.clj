a0d6670aa4cf5406aa4f6b792df9f97948da17cc
Sahil Bajaj <sahil29@gmail.com>
2015-12-24 01:34:58 +0800

    Add some static responses

--- src/jekyll_hyde/handler.clj
+++ src/jekyll_hyde/handler.clj
@@ -1,11 +1,31 @@
 (ns jekyll-hyde.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.json :as middleware]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.util.response :refer [response]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+       (response
+         {:data "Hello world"}))
+
+  (GET "/posts" []
+       (response
+         {:data [{:id 1 :contents "Today went well"}
+                 {:id 2 :contents "Today was not that great"}
+                 {:id 3 :contents "Whatever"}]}))
+
+  (GET "/config" []
+       (response
+         {:data {:highlighter :rouge
+                 :markdown :kramdown
+                 :watch true}}))
+
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response)
+      (wrap-defaults api-defaults)))
